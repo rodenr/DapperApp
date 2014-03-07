caabd5476d8ce25e76797e8a259e743fa3925be0
rodenr <ryan.roden@colorado.edu>
2014-03-06 20:52:37 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 DapperApp::Application.routes.draw do
+  get "users/new"
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
