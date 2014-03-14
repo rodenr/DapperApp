48c3ce2e698111941f61e414c46105de23ebe027
rodenr <ryan.roden@colorado.edu>
2014-03-14 14:55:36 -0700

    up to 7.4.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the coolest effing website on the interwebs"
+
       redirect_to @user
     else
       render 'new'
