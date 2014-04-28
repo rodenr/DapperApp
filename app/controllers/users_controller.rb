ac730ad3914d9c2115ad1ddaad04eab4bb2eb44b
rodenr <ryan.roden@colorado.edu>
2014-04-28 08:22:30 -0700

    skipping 10.3.3 for now, come back after friendships are complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,12 +53,12 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    #def signed_in_user
+     # unless signed_in?
+     #   store_location
+      #  redirect_to signin_url, notice: "Please sign in."
+     # end
+   # end
 
     def correct_user
       @user = User.find(params[:id])
